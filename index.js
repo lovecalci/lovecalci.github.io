25aabc2d49710e1796b833bedbbdecad467a8d4e
lovecalci <65998143+lovecalci@users.noreply.github.com>
2020-05-27 07:02:15 +0100

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-var randomNumber=Math.floor(Math.random()*100)+1;
-document.querySelector("button").addEventListener("click",function(){
-  document.querySelector("h1").innerHTML="💘 Strength  of your love is"  + randomNumber+"%";
-})
